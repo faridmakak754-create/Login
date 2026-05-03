16f13157a9bcdafd79277958e4a66f4bd4da4143
Makakmayum Farid Khan <faridmakak754@gmail.com>
2026-05-03 12:59:25 +0530

    updated

---
+++ src/main/java/p1/LogoutServlet.java
@@ -0,0 +1,27 @@
+package p1;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	
+	{
+	
+		HttpSession session=request.getSession();
+//		session.removeAttribute("a");
+		session.invalidate();
+		response.sendRedirect("index.html");
+		
+	}
+
+}
